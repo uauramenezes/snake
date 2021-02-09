3a920b6c23716cbd4d98032fdaa5a5f5bb3fc3f7
Uaurá Menezes <uaurasilva@hotmail.com>
2021-02-09 20:49:15 -0300

    Add snake movement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::io::stdout;
 use std::{thread, time};
 use crossterm::{
@@ -23,45 +24,73 @@ struct Direction {
 fn main() -> crossterm::Result<()> {
     let mut stdout = stdout();
 
-    let mut pos = Position{x: 10, y:10};
-    let mut dir = Direction{x: 1, y:0};
-
     execute!(stdout, SetSize(120, 40))?;
     terminal::enable_raw_mode()?;
     execute!(stdout, Clear(ClearType::All), EnterAlternateScreen, cursor::Hide)?;
+
+    let mut snake: VecDeque<Position> = VecDeque::new();
+
+    let mut pos = Position{x: 60, y:20};
+    let mut dir = Direction{x: 1, y:0};
+
+    for n in 0..5 {
+        let p = Position{x: pos.x - n - 1, y: pos.y};
+        snake.push_back(p);
+    }
 
     loop {
         execute!(
             stdout,
             Clear(ClearType::All),
-            cursor::MoveTo(pos.x as u16, pos.y as u16),
-            Print("\u{2588}")
         )?;
+
+        for n in 0..snake.len() {
+            execute!(
+                stdout, 
+                cursor::MoveTo(snake[n].x as u16, snake[n].y as u16),
+                Print("\u{2588}\u{2588}"),
+            )?;
+        }
 
         if poll(time::Duration::from_millis(100))? {
             match read()? {
                 Event::Key(KeyEvent {
                     code: KeyCode::Char('a'),
                     modifiers: KeyModifiers::NONE,
-                }) => {
+                }) | Event::Key(
+                    KeyEvent {
+                        code: KeyCode::Left,
+                        modifiers: KeyModifiers::NONE,
+                    }
+                ) => {
                     if dir.x == 0 {
-                        dir.x = -1;
+                        dir.x = -2;
                         dir.y = 0;
                     }
                 },
                 Event::Key(KeyEvent {
                     code: KeyCode::Char('d'),
                     modifiers: KeyModifiers::NONE,
-                }) => {
+                }) | Event::Key(
+                    KeyEvent {
+                        code: KeyCode::Right,
+                        modifiers: KeyModifiers::NONE,
+                    }
+                ) => {
                     if dir.x == 0 {
-                        dir.x = 1;
+                        dir.x = 2;
                         dir.y = 0; 
                     }
                 },
                 Event::Key(KeyEvent {
                     code: KeyCode::Char('w'),
                     modifiers: KeyModifiers::NONE,
-                }) => {
+                }) | Event::Key(
+                    KeyEvent {
+                        code: KeyCode::Up,
+                        modifiers: KeyModifiers::NONE,
+                    }
+                ) => {
                     if dir.y == 0 {
                         dir.x = 0;
                         dir.y = -1;
@@ -71,7 +100,12 @@ fn main() -> crossterm::Result<()> {
                 Event::Key(KeyEvent {
                     code: KeyCode::Char('s'),
                     modifiers: KeyModifiers::NONE,
-                }) => {
+                }) | Event::Key(
+                    KeyEvent {
+                        code: KeyCode::Down,
+                        modifiers: KeyModifiers::NONE,
+                    }
+                ) => {
                     if dir.y == 0 {
                         dir.x = 0;
                         dir.y = 1;
@@ -89,6 +123,8 @@ fn main() -> crossterm::Result<()> {
 
         pos.x += dir.x;
         pos.y += dir.y;
+        snake.pop_back();
+        snake.push_front(Position{x: pos.x, y: pos.y});
 
         if pos.x < 0 || pos.y < 0 || pos.x > 120 || pos.y > 40 {
             break
