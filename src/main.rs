8cfd61e77f378f8ed108470b5f5beac456917be0
Uaurá Menezes <uaurasilva@hotmail.com>
2021-02-09 22:23:51 -0300

    Finish basic game

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@ use std::{thread, time};
 use crossterm::{
     cursor,
     execute,
-    style::Print,
+    style::{Print, SetForegroundColor, Color, ResetColor},
     event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers},
     terminal::{
        self, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, SetSize,
     },
 };
+use rand::Rng;
 
 struct Position {
-    x: i16,
-    y: i16,
+    x: u16,
+    y: u16,
 }
 
 struct Direction {
@@ -21,16 +22,32 @@ struct Direction {
     y: i16,
 }
 
+struct Size {
+    width: u16,
+    height: u16,
+}
+
+fn get_random_xy(number: u16) -> u16 {
+    let mut rng = rand::thread_rng();
+    let mut rn = rng.gen_range(1..number);
+    while rn % 2 != 0 {
+        rn = rng.gen_range(1..number);
+    }
+    rn
+}
+
 fn main() -> crossterm::Result<()> {
     let mut stdout = stdout();
 
-    execute!(stdout, SetSize(120, 40))?;
+    let window = Size{width: 60, height: 20};
+
+    execute!(stdout, SetSize(window.width, window.height))?;
     terminal::enable_raw_mode()?;
     execute!(stdout, Clear(ClearType::All), EnterAlternateScreen, cursor::Hide)?;
 
     let mut snake: VecDeque<Position> = VecDeque::new();
 
-    let mut pos = Position{x: 60, y:20};
+    let mut pos = Position{x: window.width / 2, y: window.height / 2};
     let mut dir = Direction{x: 1, y:0};
 
     for n in 0..5 {
@@ -38,6 +55,10 @@ fn main() -> crossterm::Result<()> {
         snake.push_back(p);
     }
 
+    let mut food = Position{x: get_random_xy(window.width), y: get_random_xy(window.height)};
+
+    let mut score = 0;
+    
     loop {
         execute!(
             stdout,
@@ -47,10 +68,21 @@ fn main() -> crossterm::Result<()> {
         for n in 0..snake.len() {
             execute!(
                 stdout, 
-                cursor::MoveTo(snake[n].x as u16, snake[n].y as u16),
+                cursor::MoveTo(snake[n].x, snake[n].y),
                 Print("\u{2588}\u{2588}"),
             )?;
         }
+        
+        execute!(
+            stdout,
+            cursor::MoveTo(0, 0),
+            SetForegroundColor(Color::Red),
+            Print(format!("Score: {}", score)),
+            SetForegroundColor(Color::Green),
+            cursor::MoveTo(food.x, food.y),
+            Print("\u{2588}\u{2588}"),
+            ResetColor,
+        )?;
 
         if poll(time::Duration::from_millis(100))? {
             match read()? {
@@ -112,7 +144,7 @@ fn main() -> crossterm::Result<()> {
                     }
                 },
                 Event::Key(KeyEvent {
-                    code: KeyCode::Char('q'),
+                    code: KeyCode::Esc,
                     modifiers: KeyModifiers::NONE,
                 }) => break,
                 _ => (),
@@ -121,17 +153,41 @@ fn main() -> crossterm::Result<()> {
             ()
         }
 
-        pos.x += dir.x;
-        pos.y += dir.y;
-        snake.pop_back();
-        snake.push_front(Position{x: pos.x, y: pos.y});
+        pos.x = (pos.x as i16 + dir.x) as u16;
+        pos.y = (pos.y as i16 + dir.y) as u16;
 
-        if pos.x < 0 || pos.y < 0 || pos.x > 120 || pos.y > 40 {
+        if (snake[0].x == food.x && snake[0].y == food.y) ||
+        (snake[0].x == food.x + 1 && snake[0].y == food.y){
+            score += 10;
+            snake.push_front(Position{x: pos.x, y: pos.y});
+            food = Position{x: get_random_xy(window.width), y: get_random_xy(window.height)};
+        } else {
+            snake.pop_back();
+            snake.push_front(Position{x: pos.x, y: pos.y});
+        }
+
+        if pos.x == window.width - 1 || pos.y == window.height {
             break
         }
-        
-        thread::sleep(time::Duration::from_millis(100));
-    }   
+
+        let mut game_over = false;
+        for i in 1..snake.len() {
+            if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) ||
+            (snake[0].x + 1 == snake[i].x && snake[0].y == snake[i].y) {
+                game_over = true;
+            }
+        }
+
+        if game_over {break}
+    }
+
+    execute!(
+        stdout,
+        cursor::MoveTo(window.width / 2 - 5, window.height / 2 - 5),
+        Print("GAME OVER")
+    )?;
+
+    thread::sleep(time::Duration::from_secs(1));
 
     execute!(
         stdout,
@@ -145,4 +201,3 @@ fn main() -> crossterm::Result<()> {
 
     Ok(())
 }
-
